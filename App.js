ab0b60209ffdec1ef7613b57d27f9b80c77f9725
PeteTrapani <109101555+PeteTrapani@users.noreply.github.com>
2022-10-03 16:51:56 -0500

    Master?

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import recentGames from './Structures/FetchAPI2';
+import Header from './Structures/Header';
+
+function App() {
+  return (
+    <div className="App">App
+      <personaName />
+    </div>
+  )
+}
+
+export default App
