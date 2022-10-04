c97005522b95178ec7880b6f3e8433aab3e24371
PeteTrapani <109101555+PeteTrapani@users.noreply.github.com>
2022-10-04 08:56:11 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "./App.css";
+import recentGames from './Structures/RecentGames';
+import Header from './Structures/Header';
+
+function App() {
+  return (
+    <div className="App">App
+      <h1>useEffect</h1>
+      {/* <button onClick={() => }</button> */}
+    </div>
+  )
+}
+
+export default App
